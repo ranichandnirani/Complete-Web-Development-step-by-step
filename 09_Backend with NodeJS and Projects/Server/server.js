5219e97a040f2f84e180ce7234a74ca4f2f25f3a
ranichandnirani <chandnirani229@gmail.com>
2026-02-23 11:13:42 +0530

    build and usderdtand own Nginx server

--- 09_Backend with NodeJS and Projects/Server/server.js
+++ 09_Backend with NodeJS and Projects/Server/server.js
@@ -4,32 +4,37 @@ const path = require("path");
 
 const port = 3000;
 
+// creat a server
+
 const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, req.url === '/' ? "index.html" : "req.url");
+    const filePath = path.join(__dirname, req.url === '/' ? "index.html" : req.url);
 
-    const extName = path.extname(filePath).toLocaleLowerCase();
+    console.log(filePath);
+    // extention name
+    const extName = String(path.extname(filePath)).toLowerCase();
 
-
+    // allow file type supporting
     const mimeTypes = {
         '.html': 'text/html',
         '.css': 'text/css',
         '.js': 'text/javascript',
-        '.png': 'text/png',
+        '.png': 'image/png',
 
     };
     const contentType = mimeTypes[extName] || 'application/octet-stream';
 
+    // fs read something from file and serve that 
     fs.readFile(filePath, (err, content) => {
         if(err) {
             if(err.code === "ENOENT") {
                 res.writeHead(404, {"Content-Type": "text/html"});
-                res.end("404: File Not Found BRoooo");
+                res.end("404: File Not Found");
             } else {
                 res.writeHead(500);
                 res.end(`Server Error: ${err.code}`);
             }
         } else {
-            res.writeHead(200, {"content-type": contentType});
+            res.writeHead(200, { "content-type": contentType});
             res.end(content, "utf-8");
         }
     });
