82de9a40a890299e222420078f8b272072ea0d48
ranichandnirani <chandnirani229@gmail.com>
2026-02-26 18:37:13 +0530

    manage files

---
+++ 0006_Learn JS/JS Foundation/part1/logical.js
@@ -0,0 +1,14 @@
+//Logical Operators
+//&& -> AND
+//|| -> OR
+//!  -> NOT
+
+let isLoggedIn = true;
+let ispaid = true;
+
+console.log(isLoggedIn && ispaid); //true
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser); //true
