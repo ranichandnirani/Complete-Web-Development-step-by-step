300a39c8725dab45cd45c782b134bdb53783f03c
Chandni Rani <chandnirani229@gmail.com>
2026-03-13 17:14:00 +0530

    API response and errors, data in constants and connect with mongoDB

---
+++ 0011_Complete Backend-New-Addition/Progect management/src/utils/api-error.js
@@ -0,0 +1,26 @@
+// API ERRORS
+
+class ApiError extends Error {
+    constructor(statusCode, message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        // Error class(Utilize the error)
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        // Check stack if is it avilable
+
+        if(stack) {
+           this.stack = stack; 
+        } else {
+            Eroor.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export {ApiResponse};
