6d21436dd0a8e85ba6f65efde69a63e489207e62
Chandni Rani <chandnirani229@gmail.com>
2026-03-17 14:13:58 +0530

    catch error with async handler

--- 0011_Complete Backend-New-Addition/Progect management/src/controllers/healthcheck.controllers.js
+++ 0011_Complete Backend-New-Addition/Progect management/src/controllers/healthcheck.controllers.js
@@ -1,13 +1,21 @@
 import ApiResponse from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res)=> {
-    try {
-        res
-        .status(200)
-        .json(new ApiResponse(200, {message: "Server is runnig"}));
-    } catch (error) {
+// const healthCheck = async(req, res, next)=> {
+//     try {
+//         const user = await getUserFromDB()
+//         res
+//         .status(200)
+//         .json(new ApiResponse(200, {message: "Server is runnig"}));
+//     } catch (error) {
+//         next(err)
+//     }
+// };
 
-    }
-};
+const healthCheck = asyncHandler(async (req, res) => {
+    res
+    .status(200)
+    .json(new ApiResponse(200, {message: "Server is  running"}));
+});
 
 export { healthCheck };
