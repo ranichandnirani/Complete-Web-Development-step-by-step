2d32edaa2f1f5f8d16a26403af6f1c503ff0aa11
Chandni Rani <chandnirani229@gmail.com>
2026-04-06 21:40:18 +0530

    write aggrigation pipeline in project controllers

--- 0011_Complete Backend-New-Addition/Progect management/src/controllers/project.controllers.js
+++ 0011_Complete Backend-New-Addition/Progect management/src/controllers/project.controllers.js
@@ -8,7 +8,59 @@ import mongoose  from "mongoose";
 import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async(req, res) => {
+    const project = await ProjectMember.aggregate([
+        {
+            $match: {
+                user: new mongoose.Types.ObjectId(req.user._id),
+            },
+        },
+        {
+            $lookup: {
+                from: "projects",
+                localField: "projects",
+                foreignField: "_id",
+                as: "project",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "projectmembers",
+                            localField: "_id",
+                            foreignField: "projects",
+                            as: "projectmembers"
+                        }
+                    },
+                    {
+                        $addFields: {
+                            members: {
+                                $size: "$projectmembers",
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $unwind: "$project"
+        },
+        {
+            $project: {
+                project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    members: 1,
+                    createdAt: 1,
+                    createdBy: 1
+                },
+                role: 1,
+                _id: 0
+            },
+        },
+    ]);
 
+    return res
+      .status(200)
+      .json(new ApiResponse(200, project, "Project fetched successfully"));
 });
 
 const getProjectById = asyncHandler(async(req, res) => {
