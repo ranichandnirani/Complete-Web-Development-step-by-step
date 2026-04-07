fd637585a17ca0a99985108afc0897db49868aae
Chandni Rani <chandnirani229@gmail.com>
2026-04-07 21:23:48 +0530

    write aggregation for add member to project

--- 0011_Complete Backend-New-Addition/Progect management/src/controllers/project.controllers.js
+++ 0011_Complete Backend-New-Addition/Progect management/src/controllers/project.controllers.js
@@ -64,7 +64,16 @@ const getProjects = asyncHandler(async(req, res) => {
 });
 
 const getProjectById = asyncHandler(async(req, res) => {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId);
 
+    if(!project) {
+        throw ApiError(404, "Project not found");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, project, "Project fetched successfully"));
 });
 
 const createProject = asyncHandler(async(req, res) => {
@@ -124,6 +133,31 @@ const deleteProject = asyncHandler(async(req, res) => {
 });
 
 const addMembersToProject = asyncHandler(async(req, res) => {
+    const { email, role } = req.body;
+    const { projectId } = req.params;
+    const user = await User.findOne({email});
+
+    if(!user) {
+        throw ApiError(404, "User does not existts");
+    }
+
+    await ProjectMember.findByIdAndUpdate(
+        {
+            user: new mongoose.Types.ObjectId(user,_id),
+            project: new mongoose.Types.ObjectId(projectId)
+        },
+        {
+            user: new mongoose.Types.ObjectId(user,_id),
+            project: new mongoose.Types.ObjectId(projectId),
+            role: role
+        },
+        {
+            new: true,
+            upsert: true
+        },
+    );
+
+    return res.status(201).json(new ApiResponse(201, {}, "Project member added seccessfully"));
 
 });
 
