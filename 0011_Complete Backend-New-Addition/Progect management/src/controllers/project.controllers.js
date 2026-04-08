3437d909664842cefd9a68a021248a5dc532c4b5
Chandni Rani <chandnirani229@gmail.com>
2026-04-08 22:00:08 +0530

    controllers for project modals done

--- 0011_Complete Backend-New-Addition/Progect management/src/controllers/project.controllers.js
+++ 0011_Complete Backend-New-Addition/Progect management/src/controllers/project.controllers.js
@@ -5,7 +5,7 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import mongoose  from "mongoose";
-import { UserRolesEnum } from "../utils/constants.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async(req, res) => {
     const project = await ProjectMember.aggregate([
@@ -162,15 +162,127 @@ const addMembersToProject = asyncHandler(async(req, res) => {
 });
 
 const getProjectMembers = asyncHandler(async(req, res) => {
+    const { projectId } = req.params;
 
+    const project = await Project.findById(req.params);
+
+    if(!project) {
+        throw new ApiError(404, "Project not found");
+
+    }
+
+    const projectMembers = await ProjectMember.aggregate([
+        {
+            $match: {
+                project: new mongoose.Types.ObjectId(projectId),
+            },
+        },
+
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 1,
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1
+                        },
+                    },
+                ],
+            },
+        },
+
+        {
+            $addFields: {
+                user: {
+                    $arrayElemAt: ["$user", 0]
+                }
+            }
+        },
+        {
+            $project: {
+                project: 1,
+                role: 1,
+                user: 1,
+                updatedAt: 1,
+                createdAt: 1,
+                _id: 0,
+
+            },
+        },
+    ]);
+
+    return res 
+      .status(200)
+      .json(
+        new ApiResponse(200, projectMembers, "Project members fetched"));
 });
 
-const udateMembersRole = asyncHandler(async(req, res) => {
+const updateMembersRole = asyncHandler(async(req, res) => {
+    const { projectId, userId } = req.params;
+    const { newRole } = req.body;
+    
+    if(!AvailableUserRole. includes(newRole)) {
+        throw new ApiError(400, "Invalides Role");
+    }
 
+    let projectMember = await ProjectMember.findOne({
+        project: new mongoose.Types.ObjectId(projectId),
+        user: new mongoose.Types.ObjectId(userId),
+    });
+
+    if(!projectMember) {
+        throw new ApiError(400, "Project member not found");
+    }
+
+    projectMember = await ProjectMember.findByIdAndUpdate(
+        projectMember._id,
+        {
+            role: newRole
+        },
+        {new: true}
+    );
+
+    if(!projectMember) {
+        throw new ApiError(400, "Project member not found");
+    }
+
+    return res 
+      .status(200)
+      .json(
+        new ApiResponse(200, projectMember, "Project members role updated successfully"));
 });
 
 const deleteMember = asyncHandler(async(req, res) => {
+    const { projectId, userId } = req.params;
+    
+    let projectMember = await ProjectMember.findOne({
+        project: new mongoose.Types.ObjectId(projectId),
+        user: new mongoose.Types.ObjectId(userId),
+    });
 
+    if(!projectMember) {
+        throw new ApiError(400, "Project member not found");
+    }
+
+    projectMember = await ProjectMember.findByIdAndDelete(
+        projectMember._id,
+
+    );
+
+    if(!projectMember) {
+        throw new ApiError(400, "Project member not found");
+    }
+
+    return res 
+      .status(200)
+      .json(
+        new ApiResponse(200, projectMember, "Project members deleted successfully"));
 });
 
 export {
@@ -182,5 +294,5 @@ export {
     getProjectMembers,
     updateProject,
     deleteProject,
-    udateMembersRole,
+    updateMembersRole,
 };
