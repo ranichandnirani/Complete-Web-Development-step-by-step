85a953ca1f18d1955648f5eb1e04272dc8f7d5ea
Chandni Rani <chandnirani229@gmail.com>
2026-04-12 20:09:10 +0530

    aggregation pipeline for get task by id

--- 0011_Complete Backend-New-Addition/Progect management/src/controllers/task.controllers.js
+++ 0011_Complete Backend-New-Addition/Progect management/src/controllers/task.controllers.js
@@ -8,6 +8,7 @@ import { asyncHandler } from "../utils/async-handler.js";
 import mongoose  from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 import { MIMEType } from "util";
+import { pipeline } from "stream";
 
 const getTasks = asyncHandler(async(req, res) => {
     const { projectId } = req.params;
@@ -66,6 +67,81 @@ const createTask = asyncHandler(async(req, res) => {
 });
 
 const getTaskById = asyncHandler(async(req, res) => {
+    const { taskId } = req.params;
+    const task = await Task.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(taskId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "assigned",
+                foreignField: "_id",
+                as: "assignedTo",
+                pipeline: [
+                    {
+                        _id: 1,
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1
+                    },
+                ],
+            },
+        },
+        {
+            $lookup: {
+                from: "subtasks",
+                localField: "_id",
+                foreignField: "task",
+                as: "subtaasks",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "acreatedBy",
+                            foreignField: "_id",
+                            as: "createdBy",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        _id: 1,
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        $addFields: {
+                            createdBy: {
+                                $arrayElemAt: ["$createdBy", 0]
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                assignedTo: {
+                    $arrayElemAt: ["assignedTo", 0]
+                },
+            },
+        },
+    ]);
+
+    if(!task || task.length === 0) {
+        throw new ApiError(404, "Task not found")
+    }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, task[0], "Task fetched successfully"),
+        );
 });
 
 const updateTask = asyncHandler(async(req, res) => {
