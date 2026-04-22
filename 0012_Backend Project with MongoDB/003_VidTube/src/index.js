e84c0df612559ca050459932dea0a1cdde087753
Chandni Rani <chandnirani229@gmail.com>
2026-04-22 19:49:10 +0530

    connect database in MERN

--- 0012_Backend Project with MongoDB/003_VidTube/src/index.js
+++ 0012_Backend Project with MongoDB/003_VidTube/src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello from backend course.");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+});
+
+const PORT = process.env.PORT || 8001
+
+connectDB()
+.then(() => {
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+    
+})
+.catch((err) => {
+    console.log("MongoDB connection error", err)
+})
