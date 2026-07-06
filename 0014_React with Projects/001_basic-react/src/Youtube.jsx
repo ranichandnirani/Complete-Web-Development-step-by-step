67e3a22cb0c096396deb3ff898fd8402ecaab607
Chandni Rani <chandnirani229@gmail.com>
2026-07-06 17:19:44 +0530

    Understanding react flow and structure

---
+++ 0014_React with Projects/001_basic-react/src/Youtube.jsx
@@ -0,0 +1,10 @@
+function Youtube() {
+    return(
+        <>
+            <h1>This is Youtube app</h1>
+    
+        </>
+    )
+}
+
+export default Youtube
---
+++ 0014_React with Projects/001_basicvite/src/youtub.jsx
@@ -0,0 +1,10 @@
+function Youtub() {
+    return(
+        <div>
+            <h1>Vite react youtube app</h1>
+            <h1>Vite react app</h1>
+        </div>
+    )
+}
+
+export default Youtub
