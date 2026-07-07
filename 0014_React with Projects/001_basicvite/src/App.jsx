169328bbab66c2f7e8da2b236b6d64ad37b60762
Chandni Rani <chandnirani229@gmail.com>
2026-07-07 17:50:41 +0530

    Creat own react librery and JSX

--- 0014_React with Projects/001_basicvite/src/App.jsx
+++ 0014_React with Projects/001_basicvite/src/App.jsx
@@ -3,12 +3,13 @@ import Youtub from './youtub.jsx'
 
 
 function App() {
+  const username = "Chandni"
   return (
-    <>
+    <div>
       <h1>Vite react App</h1>
-      <h1>Vite react App</h1>
+      <h1>Vite react App {username}</h1>
       <Youtub />
-    </>
+    </div>
   )
 }
 
--- 0014_React with Projects/001_basicvite/src/main.jsx
+++ 0014_React with Projects/001_basicvite/src/main.jsx
@@ -1,8 +1,37 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    children: 'Click me!'
+}
+
+function MyApp() {
+    return(
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://www.google.com" target="_blank"> Google </a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {
+        href: 'https://www.google.com',
+        target: '_blank',
+    },
+    'Click me!'
+)
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    <MyApp />
 )
