5f6dd17293e956a9860bd4f3285d5eef5eeb75b0
Chandni Rani <chandnirani229@gmail.com>
2026-07-08 16:57:11 +0530

    why need hooks and project

---
+++ 0014_React with Projects/002_counter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>React course with Hitesh</h1>
+      <h2>Couter value:</h2>
+      <button>add value</button> {"  "}
+      <button>remove value</button>
+      <p>footer:</p>
+    </>
+  )
+}
+
+export default App
