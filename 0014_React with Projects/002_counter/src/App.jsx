a6e3cda6b963e2a88e2576a2ef2d7f20361dc7b5
Chandni Rani <chandnirani229@gmail.com>
2026-07-09 21:52:20 +0530

    why need hooks and project

--- 0014_React with Projects/002_counter/src/App.jsx
+++ 0014_React with Projects/002_counter/src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  let counter = 15
+
+  const addValue = () => {
+
+    counter = counter += 1
+    console.log(counter)
+  }
+
+  const removeValue = () => {
+    counter = counter -= 1
+    console.log(counter)
+  }
+
   return (
     <>
-      <h1>React course with Hitesh</h1>
-      <h2>Couter value:</h2>
-      <button>add value</button> {"  "}
-      <button>remove value</button>
-      <p>footer:</p>
+      <h1>React course with Hitesh {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>add value</button> {"  "}
+      <button onClick={removeValue}>remove value</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
