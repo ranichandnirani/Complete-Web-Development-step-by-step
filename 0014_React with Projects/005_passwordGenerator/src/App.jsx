6bd6e8d628b454e3ac59096211baeaf8898b4251
Chandni Rani <chandnirani229@gmail.com>
2026-07-18 20:13:45 +0530

    Some changes

--- 0014_React with Projects/005_passwordGenerator/src/App.jsx
+++ 0014_React with Projects/005_passwordGenerator/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*-_+"
+    if (charAllowed) str += "!@#$%^&*-()_+"
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
@@ -24,9 +24,7 @@ function App() {
     }
 
     setPassword(pass)
-
-
-  }, [length, numberAllowed, charAllowed, setPassword])
+  }, [length, numberAllowed, charAllowed])
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
