306a8a5a1f102d1359c9ae64316a0fff9ee2f0e5
Chandni Rani <chandnirani229@gmail.com>
2026-07-22 20:17:45 +0530

    user and github page

--- 0014_React with Projects/007_reactRouter/src/main.jsx
+++ 0014_React with Projects/007_reactRouter/src/main.jsx
@@ -7,13 +7,20 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import Github, {githubInfoLoader} from './components/GitHub/Github.jsx'
+import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />}/>
-      <Route path="about/" element={<About />}></Route>
-      <Route path="contact/" element={<Contact />}></Route>
+      <Route path='' element={<Home />} />
+      <Route path="about/" element={<About />} />
+      <Route path="contact/" element={<Contact />} />
+      <Route loader={githubInfoLoader} path='github' element={<Github />} />
+      <Route path="user/" element={<User />}>
+        <Route path=':userid/' element={<User />} />
+      <Route path="*" element={<div>Not Found</div>} />
+      </Route>
     </Route>
   )
 )
